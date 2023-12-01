42a136061f03e533feb5065fcb1fcefbd44899fd
Mario.Rios <qvoiekpjzzvjepg35rrsarhcyx2rcx3y5kf6oa7ygz6bycfyfvyq>
2023-12-01 14:31:00 -0500

    url

--- config/constants.ts
+++ config/constants.ts
@@ -1,2 +1,2 @@
 export const CLIENT_ID = '1234-abcd';
-export const TWO_FACTOR_AUTHENTICATION_DOMAIN = 'http://127.0.0.1:9812';
+export const TWO_FACTOR_AUTHENTICATION_DOMAIN = 'http://129.146.71.229:8000';
